bcd408125281b162b6beb6a02332427786d4532c
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2021-11-12 17:03:38 +0530

    Added the Problem Statement for Day1

--- Day1.js
+++ Day1.js
@@ -1,3 +1,5 @@
+//Problem Statement: Create a function which returns a random number in the given range of values both inclusive
+
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
 	return Math.floor(Math.random() * ((rangeEnd - rangeStart ) + 1) ) + rangeStart
 }
